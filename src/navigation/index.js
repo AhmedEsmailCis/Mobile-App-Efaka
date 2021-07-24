4bbdd956139601d855de9bec6176d09d415cab8d
AhmedEsmailCis <ahmed.esmail.fcis.2019@gmail.com>
2021-07-24 13:32:42 +0200

    add splash screen

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,11 +1,15 @@
-import * as React from "react";
+import React, { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import SplashScreen from "react-native-splash-screen";
 import DrawerNavigator from "./DrawerNavigator";
 import { Login, ScanningBarcode, SignUp, Starting } from "../screens";
 
 const Stack = createStackNavigator();
 export default function StackNavigator() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <NavigationContainer>
       <Stack.Navigator>
